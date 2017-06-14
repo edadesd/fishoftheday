9f6437ddc3191bcbcd311d4c7e62961da3185c4d
edadesd <edadesd@oregonstate.edu>
2017-06-13 19:18:07 -0700

    Added data files and scripts.

---
+++ resetWindow.py
@@ -0,0 +1,4 @@
+filename = "/home/pi/everyFish/windowReplies.txt"
+
+with open(filename, "w") as resetFile:
+    resetFile.write("0")
---
+++ runfish.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+for i in 1 2 3;
+do
+	python /home/pi/everyFish/youCaught.py
+	/bin/sleep 20
+done
