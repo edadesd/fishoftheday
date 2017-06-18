d9f80bd3e7903bae14598ba7057408a8800f3f52
edadesd <edadesd@oregonstate.edu>
2017-06-17 22:24:54 -0700

    Commented fotd.py

--- fotd.py
+++ fotd.py
@@ -8,12 +8,18 @@ from fishes import fishes
 
 random.seed()
 
+#Authenticate and setup tweepy API object
+
 auth = tweepy.OAuthHandler(CONSUMER_TOKEN, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
 api = tweepy.API(auth)
 
-filename = "/home/pi/everyFish/fishleft.txt"
+#Change this to the path to your fishleft.txt file
+filename = "/home/everyFish/fishleft.txt"
+
+''' Read the fishleft file line by line
+and choose a random fish to post.'''
 
 with open(filename, "r") as fishleft:
     remainingIndices = fishleft.readlines()
@@ -22,14 +28,24 @@ chosen = random.choice(remainingIndices)
 chosenStripped = chosen.rstrip("\n")
 fotdIndex = int(chosenStripped)
 
+'''Take the fish's
+name and url entries from the chosen fish's
+dict.'''
 
 fotdName = fishes[fotdIndex]['name']
 fotdUrl = fishes[fotdIndex]['url']
+
+#Format the post and send it.
 
 postString = "Fish of the Day: " + "\n" + fotdName + "\n" + fotdUrl
 print postString
 
 api.update_status(postString)
+
+'''Write all of the indices back to fishleft
+except for the one that was chosen. This has
+the effect of removing the chosen index from the
+file so it will not be repeated on later executions.'''
 
 with open(filename, "w") as fishleft:
 	for i in remainingIndices:
