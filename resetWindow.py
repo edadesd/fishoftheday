0e1f3ca60164df62af775d4fb61509004374df25
edadesd <edadesd@oregonstate.edu>
2017-06-17 22:26:45 -0700

    Added comments to resetWindow.py

--- resetWindow.py
+++ resetWindow.py
@@ -1,4 +1,9 @@
-filename = "/home/pi/everyFish/windowReplies.txt"
+'''Resets the tracked number of replies
+stored in windowReplies.txt to zero to indicate
+the start of a new fifteen minute window.'''
+
+
+filename = "/home/everyFish/windowReplies.txt"
 
 with open(filename, "w") as resetFile:
     resetFile.write("0")
