6b4e9886e128fbb9395170139839e3989eb4b781
edadesd <edadesd@oregonstate.edu>
2017-06-17 22:27:47 -0700

    Added comments to secret.py

--- secret.py
+++ secret.py
@@ -1,3 +1,8 @@
+'''Add these values from the Twitter app
+page so they can kept separate from the actual
+bot itself. The bot will read them from this file
+when it needs to authenticate.'''
+
 CONSUMER_TOKEN = ""
 CONSUMER_SECRET = ""
 ACCESS_TOKEN = ""
