12d799ceff1ab7416638161f1e3970edb6051dc6
Daniel Edades <edadesd@oregonstate.edu>
2017-08-04 02:49:47 -0700

    Comments for new version of fotd.py

--- fotd.py
+++ fotd.py
@@ -17,10 +17,16 @@ auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
 api = tweepy.API(auth)
 
+# Make a list of fish that have not already been posted
+# by iterating through the list of fish and extracting
+# the ones that have not been already.
+
 validFish = []
 for fish in fishes:
         if fish['available'] == "True":
                 validFish.append(fish)
+
+# Choose a fish from the available fish and post its contents.
 
 fotd = random.choice(validFish)
 fotdIndex = fotd['index']
@@ -30,8 +36,15 @@ postString = "Fish of the Day: " + "\n" + fotd['name']+ "\n" + fotd['url']
 
 api.update_status(postString)
 
+# Update the chosen fish to indicate it has already been posted.
+# The json.dumps() method turns the Python boolean value "True"
+# into "true" so this uses the string.
+
 fishes[fotdIndex]['available'] = "False"
 # print fishes[fotdIndex]
+
+# Write each of the fish into the file by dumping its dict as a JSON-formatted
+# string.
 
 with open(fishfile, "w") as fishFile:
         fishFile.write("fishes = [\n")
@@ -45,3 +58,4 @@ with open(fishfile, "w") as fishFile:
                 fishFile.write(fishString)
                 index += 1
         fishFile.write("]")
+        
