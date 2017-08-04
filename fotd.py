8630068378d4a3447406890f8791fb9ffb603aab
Daniel Edades <edadesd@oregonstate.edu>
2017-08-04 02:03:00 -0700

    Changed FOTD to rely on dict attribute.

--- fotd.py
+++ fotd.py
@@ -8,44 +8,23 @@ from fishes import fishes
 
 random.seed()
 
-#Authenticate and setup tweepy API object
-
 auth = tweepy.OAuthHandler(CONSUMER_TOKEN, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
 api = tweepy.API(auth)
 
-#Change this to the path to your fishleft.txt file
-filename = "/home/everyFish/fishleft.txt"
+validFish = []
+for fish in fishes:
+        if fish['available'] == "True":
+                validFish.append(fish)
 
-''' Read the fishleft file line by line
-and choose a random fish to post.'''
+fotd = validfish.choice()
 
-with open(filename, "r") as fishleft:
-    remainingIndices = fishleft.readlines()
 
-chosen = random.choice(remainingIndices)
-chosenStripped = chosen.rstrip("\n")
-fotdIndex = int(chosenStripped)
-
-'''Take the fish's
-name and url entries from the chosen fish's
-dict.'''
-
-fotdName = fishes[fotdIndex]['name']
-fotdUrl = fishes[fotdIndex]['url']
-
-#Format the post and send it.
-
-postString = "Fish of the Day: " + "\n" + fotdName + "\n" + fotdUrl
+postString = "Fish of the Day: " + "\n" + fotd['name']+ "\n" + fotd['url']
 print postString
 
 api.update_status(postString)
-
-'''Write all of the indices back to fishleft
-except for the one that was chosen. This has
-the effect of removing the chosen index from the
-file so it will not be repeated on later executions.'''
 
 with open(filename, "w") as fishleft:
 	for i in remainingIndices:
