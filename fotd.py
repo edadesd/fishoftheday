888c6032da971b7491e8466b7cc546bd2d3a5416
Daniel Edades <edadesd@oregonstate.edu>
2017-08-04 03:19:35 -0700

    Cast each dict as str instead of using json.dumps

--- fotd.py
+++ fotd.py
@@ -2,10 +2,8 @@
 
 import tweepy
 import random
-import json
 from secret import CONSUMER_TOKEN, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET
 from fishes import fishes
-
 
 fishfile = "/home/pi/everyFish/fishes.py"
 
@@ -22,9 +20,9 @@ api = tweepy.API(auth)
 
 validFish = []
 for fish in fishes:
-        if fish['available'] == "True":
+        if fish['available'] == True:
                 validFish.append(fish)
-
+        
 # Choose a fish from the available fish and post its contents.
 
 fotd = random.choice(validFish)
@@ -36,10 +34,8 @@ postString = "Fish of the Day: " + "\n" + fotd['name']+ "\n" + fotd['url']
 api.update_status(postString)
 
 # Update the chosen fish to indicate it has already been posted.
-# The json.dumps() method turns the Python boolean value "True"
-# into "true" so this uses the string.
 
-fishes[fotdIndex]['available'] = "False"
+fishes[fotdIndex]['available'] = False
 # print fishes[fotdIndex]
 
 # Write each of the fish into the file by dumping its dict as a JSON-formatted
@@ -49,7 +45,7 @@ with open(fishfile, "w") as fishFile:
         fishFile.write("fishes = [\n")
         index = 0
         for fish in fishes:
-                fishString = json.dumps(fish)
+                fishString = str(fish)
                 if index < len(fishes) - 1:
                         fishString += ",\n"
                 else:
@@ -57,4 +53,3 @@ with open(fishfile, "w") as fishFile:
                 fishFile.write(fishString)
                 index += 1
         fishFile.write("]")
-        
